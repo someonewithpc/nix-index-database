3b606b4db05087553aad62a2d8ba3fc98e599c20
Hugo Sales <hugo@hsal.es>
2025-04-15 19:12:13 +0100

    Replace default.nix to match nixos-modules.nix, without using self

--- default.nix
+++ default.nix
@@ -1,36 +1,15 @@
-{ pkgs, ... }:
-let
-  generated = import ./generated.nix;
-
-  nix-index-database =
-    (pkgs.fetchurl {
-      url = generated.url + pkgs.stdenv.system;
-      hash = generated.hashes.${pkgs.stdenv.system};
-    }).overrideAttrs
-      {
-        __structuredAttrs = true;
-        unsafeDiscardReferences.out = true;
-      };
-
-  nix-index-small-database =
-    (pkgs.fetchurl {
-      url = generated.url + pkgs.stdenv.system + "-small";
-      hash = generated.hashes."${pkgs.stdenv.system}-small";
-    }).overrideAttrs
-      {
-        __structuredAttrs = true;
-        unsafeDiscardReferences.out = true;
-      };
-in
 {
-  inherit nix-index-database nix-index-small-database;
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  imports = [ ./nix/shared.nix ];
 
-  nix-index-with-db = pkgs.callPackage ./nix-index-wrapper.nix { inherit nix-index-database; };
-  nix-index-with-small-db = pkgs.callPackage ./nix-index-wrapper.nix {
-    nix-index-database = nix-index-small-database;
-    db-type = "small";
-  };
-  comma-with-db = pkgs.callPackage ./comma-wrapper.nix {
-    nix-index-database = nix-index-small-database;
-  };
+  programs.nix-index.package = lib.mkDefault pkgs.nix-index-with-db;
+
+  programs.command-not-found.enable = lib.mkDefault false;
+  environment.systemPackages = lib.mkIf config.programs.nix-index-database.comma.enable
+    [ pkgs.comma-with-db ];
 }
